e3420259f62e2d7c4e2c5a4971a4222dafcfb2dc
moses igbuku <captainoak@gmail.com>
2017-12-16 11:42:40 +0100

    added home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 import { AppComponent } from './app.component';
 
+const routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full'},
+  { path: 'home', component: HomeComponent },
+  { path: '**', redirectTo: 'auth'}
+];
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
